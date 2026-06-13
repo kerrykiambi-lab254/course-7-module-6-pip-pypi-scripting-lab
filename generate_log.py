3a6b91f9c95dbc210baf5534bc50426868ee0b55
kerrykiambi-lab254 <kerry.kiambi@student.moringaschool.com>
2026-06-13 07:40:34 +0300

    Add root generate_log entry point

---
+++ generate_log.py
@@ -0,0 +1,6 @@
+from lib.generate_log import generate_log
+
+
+if __name__ == "__main__":
+    sample_data = ["User logged in", "User updated profile", "Report exported"]
+    generate_log(sample_data)
